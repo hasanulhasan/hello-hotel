db92ea856f275264a9d05693bc2e4cc2b2092d89
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-07 13:05:16 +0600

    header part is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-width: 1280px">
+      <Navbar></Navbar>
+      <Hero></Hero>
       <h1>Hello Hotel ,, a hotel booking app</h1>
     </div>
   );
